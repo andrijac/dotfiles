8ea0e53baba6db3738fb85a6bc873934c1b413c8
Carles Fenollosa <carlos.fenollosa@gmail.com>
2012-08-31 16:43:50 +0200

    New battery script

--- bin/battery.sh
+++ bin/battery.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 source $HOME/.bash_functions
-require_machine fightclub.local && echo "| `pmset -g ps | sed 's/;/ /g' | tail -n 1 | awk '{print $2 " ("$4")"}'` | "
+require_machine fightclub.local && echo "| `pmset -g ps | sed 's/;/ /g' | tail -n 1 | awk '{printf ("%s", $2); if ($4 == "(no") { print " (wait)" } else { print " ("$4")"} }'` | "
